312ed5349e1613243f7a6818d9e20ea24de6e55a
Kathy Kang <105574653+KatSKang@users.noreply.github.com>
2022-10-07 02:46:24 -0400

    updated routes, connection

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,12 +8,12 @@ router.get("/", async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Tag.findAll({
+    const productData = await Product.findAll({
       include: [
         {
           model: Category,
+          Tag,
         },
-        { through: Tag },
       ],
     });
     res.status(200).json(productData);
@@ -27,12 +27,12 @@ router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Tag.findByPk(req.params.id, {
+    const productData = await Product.findByPk(req.params.id, {
       include: [
         {
           model: Category,
+          Tag,
         },
-        { through: Tag },
       ],
     });
     res.status(200).json(productData);
@@ -115,8 +115,24 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product found with this id." });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,12 +8,7 @@ router.get("/", async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [
-        {
-          model: Product,
-        },
-        { through: ProductTag },
-      ],
+      include: [{ model: Product, ProductTag }],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -26,12 +21,7 @@ router.get("/:id", async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [
-        {
-          model: Product,
-        },
-        { through: ProductTag },
-      ],
+      include: [{ model: Product, ProductTag }],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -76,7 +66,7 @@ router.delete("/:id", async (req, res) => {
     });
 
     if (!tagData) {
-      res.status(404).json({ message: "No category found with this id." });
+      res.status(404).json({ message: "No tag found with this id." });
       return;
     }
 
